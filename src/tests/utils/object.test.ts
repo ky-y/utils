b5dc32d0df698e3e5be32e94e848c4c89cd61525
kyoya0819 <kyoya0819@gmail.com>
2024-09-20 13:00:45 +0900

    wip

---
+++ src/tests/utils/object.test.ts
@@ -0,0 +1,50 @@
+import { assertType, expect, test } from "vitest";
+
+import { getKeys, forEach, map } from "~/utils";
+
+
+test("Positive Testing: getKeys", () => {
+    const test_object_1 = { a: "hogehoge", b: "hogehoge" };
+
+    expect(getKeys(test_object_1)).toStrictEqual(["a", "b"]);
+    assertType<("a"|"b")[]>(getKeys(test_object_1));
+
+    const test_object_2 = { 1: "hogehoge", testtest: "hogehoge", piyo: "piyo" };
+
+    expect(getKeys(test_object_2)).toStrictEqual(["1", "testtest", "piyo"]);
+    assertType<(1|"testtest"|"piyo")[]>(getKeys(test_object_2));
+});
+
+
+test("Positive Testing: forEach", () => {
+    const test_object_1 = { a: "hogehoge", b: "hogehoge" };
+
+    let i = 0;
+    forEach(test_object_1, (v1, v2, v3) => {
+
+        expect(v3).toBe(i);
+        expect(v2).toBe(getKeys(test_object_1)[i]);
+        expect(v1).toBe(test_object_1[
+            getKeys(test_object_1)[i]
+            ]);
+        i++;
+    });
+});
+
+
+test("Positive Testing: map", () => {
+    const test_object_1 = { a: "hogehoge", b: "hogehoge" };
+
+    let i = 0;
+    const result = map(test_object_1, (v1, v2, v3) => {
+
+        expect(v3).toBe(i);
+        expect(v2).toBe(getKeys(test_object_1)[i]);
+        expect(v1).toBe(test_object_1[
+            getKeys(test_object_1)[i]
+            ]);
+        i++;
+        return "aiueo";
+    });
+    expect(result).toStrictEqual({ a: "aiueo", b: "aiueo" });
+});
--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,3 +1,51 @@
-export const getKeys = <T extends { [key: string]: any }>(v: T): (keyof T)[] => {
+export const getKeys = <T extends { [key: string|number]: any }>(v: T): (keyof T)[] => {
     return Object.keys(v);
+};
+
+
+export const forEach = <T extends { [key: string|number]: any }>(
+    v: T,
+    func: <V extends keyof T>(
+        value: T[V],
+        key: V,
+        index: number
+    ) => void
+): void => {
+
+    getKeys(v).forEach((key, index) => func(v[key], key, index));
+};
+
+
+export const map = <T extends { [key: string|number]: any }, V extends any>(
+    v: T,
+    func: <U extends keyof T>(
+        value: T[U],
+        key: U,
+        index: number
+    ) => V
+): { [key in keyof T]: V } => {
+
+    const result = {} as { [key in keyof T]: V };
+
+    getKeys(v).forEach((key, index) => {
+        result[key] = func(v[key], key, index);
+    });
+
+    return result;
+};
+
+
+export const forEachAsync = async (V: any) => {
+
+};
+
+
+export const mapAsync = async(V: any) => {
+
+};
+
+export const object = {
+    getKeys,
+    forEach,
+    map
 };
