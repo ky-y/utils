f2c44eccba000c449dd78a69ae72b13e379c29f1
kyoya0819 <kyoya0819@gmail.com>
2024-10-21 00:59:01 +0900

    support boolean

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,3 +1,5 @@
-export const cn = (...classNames: (string|null|undefined)[]): string => {
-    return classNames.filter(Boolean).join(" ");
+export const cn = (...classNames: (string|null|undefined|boolean)[]): string => {
+    return classNames
+        .filter(v => v !== true)
+        .filter(Boolean).join(" ");
 };
